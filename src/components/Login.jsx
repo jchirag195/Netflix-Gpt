5f6aa601b91a261ba4d92c6838e5c8ee741b92de
jchirag195 <73137787+jchirag195@users.noreply.github.com>
2025-04-22 10:42:44 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
+import { checkValidData } from "../utils/Validate.jsx";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/Firebase.jsx';
 import { useDispatch } from 'react-redux';
